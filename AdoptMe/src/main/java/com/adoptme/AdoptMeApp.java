e61f274560c949314ba368e0e5874158a8c91616
AndrewLHellman <142917243+AndrewLHellman@users.noreply.github.com>
2025-05-04 22:52:33 -0500

    Unify controller and view classes

--- AdoptMe/src/main/java/com/adoptme/AdoptMeApp.java
+++ AdoptMe/src/main/java/com/adoptme/AdoptMeApp.java
@@ -1,11 +1,5 @@
 package com.adoptme;
 
-import com.adoptme.controller.AdoptablePetsController;
-import com.adoptme.model.*;
-import com.adoptme.view.PetsView;
-import javax.swing.SwingUtilities;
-
-import java.io.*;
 import com.adoptme.controller.PetController;
 import com.adoptme.view.PetView;
 
@@ -16,40 +10,34 @@ import javax.swing.*;
  * Entry point for the application.
  */
 public class AdoptMeApp {
-	/**
+    /**
      * Main method - application entry point
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-    	Shelter<Pet> shelter = new Shelter<Pet>();
-    	shelter.addPet(new Dog(1, "Jake", "Pug", 0, false));
-    	shelter.adoptPet("Jake");
-    	System.out.println(shelter.getAllPets());
-    	
-    	try {
+        try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
             e.printStackTrace();
         }
-    	
-    	SwingUtilities.invokeLater(() -> {
-    		try {
-    			PetController controller = new PetController();
-    			
-    			PetView view = new PetView(controller);
-    			
-    			controller.initialize();
-    			
-    			view.setVisible(true);
-    		} catch (Exception e) {
-    			e.printStackTrace();
-    			JOptionPane.showMessageDialog(null,
-    					"Error starting application: " + e.getMessage(),
-    					"Application Error",
-    					JOptionPane.ERROR_MESSAGE);
-    			System.exit(1);
-    		}
-    	});
-    };
-    
+        
+        SwingUtilities.invokeLater(() -> {
+            try {
+                PetController controller = new PetController();
+                
+                PetView view = new PetView(controller);
+                
+                controller.initialize();
+                
+                view.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null,
+                        "Error starting application: " + e.getMessage(),
+                        "Application Error",
+                        JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+            }
+        });
+    }
 }
